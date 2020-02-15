9f37e3f39b88583685fc9d7566da8ee119dcf300
Asia95 <janaszek@onet.pl>
2020-02-15 20:27:15 +0000

    get all liked videos

--- main.py
+++ main.py
@@ -7,35 +7,47 @@ import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret_file.json"
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = "client_secret_file.json"
+
+# Get credentials and create an API client
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_console()
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+
+request = youtube.videos().list(
+    part="snippet,contentDetails,statistics",
+    myRating="like",
+    maxResults=50
+)
+
+response = request.execute()
+while 'nextPageToken' in response:
+    for item in response["items"]:
+        video_id = item["id"]
+        youtube_url = "https://www.youtube.com/watch?v={}".format(item["id"])
+        print(item)
 
     request = youtube.videos().list(
         part="snippet,contentDetails,statistics",
-        myRating="like"
+        myRating="like",
+        maxResults=50,
+        pageToken=response['nextPageToken']
     )
 
     response = request.execute()
-    for item in response["items"]:
-        video_title = item["snippet"]["title"]
-        print(video_title)
 
 
 
-    print(response)
+print(response)
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
