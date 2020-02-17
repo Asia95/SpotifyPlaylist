994763d17d1e9245b3078a11d014c702f16d5983
Asia95 <janaszek@onet.pl>
2020-02-17 22:13:13 +0000

    get title and artist of song and create empty spotify playlist

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
-
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import youtube_dl
+import json
+import requests
+from spotify_auth import spotify_user_id, OATH_TOKEN
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
 
 # Disable OAuthlib's HTTPS verification when running locally.
 # *DO NOT* leave this option enabled in production.
@@ -15,6 +17,19 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 api_service_name = "youtube"
 api_version = "v3"
 client_secrets_file = "client_secret_file.json"
+
+# Create Spotify Playlist
+request_body = json.dumps({"name": "Youtube Liked Videos"})
+query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
+response = requests.post(
+    query,
+    data=request_body,
+    headers={
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(OATH_TOKEN)
+    }
+)
+response_json = response.json()
 
 # Get credentials and create an API client
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -30,11 +45,14 @@ request = youtube.videos().list(
 )
 
 response = request.execute()
+ydl = youtube_dl.YoutubeDL({})
 while 'nextPageToken' in response:
     for item in response["items"]:
-        video_id = item["id"]
-        youtube_url = "https://www.youtube.com/watch?v={}".format(item["id"])
-        print(item)
+        with ydl:
+            video = ydl.extract_info("https://www.youtube.com/watch?v={}".format(item["id"]), download=False)
+
+        if video['artist'] is not None and video['track'] is not None:
+            print('{} - {}'.format(video['artist'], video['track']))
 
     request = youtube.videos().list(
         part="snippet,contentDetails,statistics",
